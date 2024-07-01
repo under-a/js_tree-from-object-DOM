b980a0b1cc8ee1730624927a1e66b24faaa3294e
under-a <mymail15@i.ua>
2024-07-02 01:04:22 +0300

    make build

---
+++ index.d801a59a.js
@@ -0,0 +1,2 @@
+!function e(n,r){var t=document.createElement("ul");for(var a in r){var l=document.createElement("li");l.textContent=a,t.append(l),n.append(t),t.children.length&&e(l,r[a])}}(document.querySelector("#tree"),{Drink:{Wine:{},Schnaps:{}},Fruit:{Red:{Cherry:{},Strawberry:{}},Yellow:{Banana:{},Pineapple:{}}}});
+//# sourceMappingURL=index.d801a59a.js.map
